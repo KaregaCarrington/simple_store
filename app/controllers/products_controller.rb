2489572ab6e3d9a28ecc43bdb3a06630bbf84e3f
Rigz777 <rigzgoodmusic@gmail.com>
2017-10-24 17:45:39 -0400

    fleshed out all views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,11 +4,45 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to root_url
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :stock_quantity)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, :description, :price, :stock_quantity, presence: true
+  validates :price, numericality: { only_integer: true }
+  validates :stock_quantity, numericality: { only_integer: true }
 end

6ea92847bd1d5cbc181f58dceae271898c7f5647
Rigz777 <rigzgoodmusic@gmail.com>
2017-10-24 16:05:28 -0400

    added product controller, actions, views and routes

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'products/index'
+
+  get 'products/new'
+
+  get 'products/show'
+
+  get 'products/edit'
+
+  resources :products
+  root 'products#index'
 end
